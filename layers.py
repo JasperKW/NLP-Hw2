360199cc37fca6c0c9c833de5014cb069dd05292
Kongjian Wang <159342919+JasperKW@users.noreply.github.com>
2024-10-17 19:38:31 -0400

    Update layers.py

--- layers.py
+++ layers.py
@@ -76,10 +76,12 @@ class DecoderLayer(nn.Module):
 def attention(query, key, value, mask=None, dropout=None):
     "Compute 'Scaled Dot Product Attention'"
     d_k = query.size(-1)
-    scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
+    scores = torch.matmul(query, key.transpose(-2, -1))
     if mask is not None:
-        scores = scores.masked_fill(mask == 0, -1e9)
-    p_attn = scores.softmax(dim=-1)
+        if mask.dim() == 2:
+            mask = mask.unsqueeze(1)
+        scores = scores.masked_fill(mask == 0, float('-inf'))
+        p_attn = scores.softmax(dim=-1)
     if dropout is not None:
         p_attn = dropout(p_attn)
     return torch.matmul(p_attn, value), p_attn
