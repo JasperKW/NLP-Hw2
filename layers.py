573cdd6281957ac8c24d1ddd39905234df162a72
Kongjian Wang <159342919+JasperKW@users.noreply.github.com>
2024-10-17 22:02:23 -0400

    Update layers.py

--- layers.py
+++ layers.py
@@ -132,6 +132,23 @@ class MultiHeadedAttention(nn.Module):
         del key
         del value
         return self.linears[-1](x)
+        
+    def attention(query, key, value, mask=None, dropout=None):
+        "Compute 'Scaled Dot Product Attention'"
+        d_k = query.size(-1)
+        scores = torch.matmul(query, key.transpose(-2, -1))
+        p_attn = None
+        if mask is not None:
+            if mask.dim() == 2:
+                mask = mask.unsqueeze(1)
+            scores = scores.masked_fill(mask == 0, float('-inf'))
+        p_attn = scores.softmax(dim=-1)
+        if dropout is not None:
+            p_attn = dropout(p_attn)
+        return torch.matmul(p_attn, value), p_attn
+
+
+   
     
     
     
